1137c1b20c3f6700f76912181544a487364b6242
John Dunning <fw@johndunning.com>
2026-02-17 18:37:47 -0800

    Add DEV to the extension name when in dev mode
    Clean up formatting of list of images with alt text errors.

--- packages/extension/src/sidepanel/components/A11yChecker.tsx
+++ packages/extension/src/sidepanel/components/A11yChecker.tsx
@@ -494,35 +494,20 @@ const ImageAltTextResults = ({ results, apiImages }: { results: ImageAltTextInfo
 					<span>
 						{missingAltText.length} {imageWord} on this page {verbWord} missing alt text.{" "}
 						{imagesInApi.length > 0 && (
-							<>
-								Look for the{" "}
-								<span className="text-amber-600 shrink-0">
-									<svg 
-										className="w-4 h-4 inline" 
-										fill="currentColor" 
-										viewBox="0 0 20 20"
-										xmlns="http://www.w3.org/2000/svg"
-									>
-										<path 
-											fillRule="evenodd" 
-											d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" 
-											clipRule="evenodd" 
-										/>
-									</svg>
-								</span>
-								{" "}under Images and documents.
-							</>
+							<span className="text-amber-600 shrink-0">
+								Look for ⚠️ under Images and documents.
+							</span>
 						)}
 					</span>
 				</p>
 			</div>
-			
+
 			{imagesNotInApi.length > 0 && (
 				<div className="p-3 bg-amber-50 text-amber-900 text-sm rounded border border-amber-100">
 					<p className="font-semibold mb-2">
 						Images not listed in "Images and documents":
 					</p>
-					<ul className="list-disc list-inside space-y-1 text-xs">
+					<ul className="list-disc list-outside pl-4 space-y-1 text-xs">
 						{imagesNotInApi.map((info, index) => (
 							<li key={index} className="break-all">
 								{info.filename || info.url}
